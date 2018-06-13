80f8ed378293f8b361987db3c779bc83a3353a9d
Ferlinuxdebian <34031083+Ferlinuxdebian@users.noreply.github.com>
2018-06-13 22:36:24 +0000

    Update sources-install.sh

--- sources-install.sh
+++ sources-install.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
+#==============================================================================
+#Título          :sources-install.sh
+#Descrição       :Script simples para adicionar repositórios.
+#Autor		 			 :Fernando Debian
+#Observações     :Necessita do dialog instalado.
+#==============================================================================
 
 #Variáveis:
-#Variável para despejar STDOUT em /etc/apt/sources.list
-ESPELHOS=/etc/apt/sources.list
+ESPELHOS=/etc/apt/sources.list #Usada nas funções para mandar saída sources.list
+#==============================================================================
+# Váriáveis na function addsources
+# SECTION: Recebe uma seção Referente para editar o cabeçalho.
+# RELEASE_LINK: Recebe a seção do repositório, stretch main, stretch-updates...
+# COMENTARIO: adiciona um comentário no repositório stretch-proposed-update.
+# sources: recebe o contudo escolhido no dialog.
+# escolha: Recebe uma opção por vez do while, para o processamento case.
+# NONFREE: Recebe non-free se for escolhido pelo usuário, útil na função main-install
+# CONTRIB: O mesmo que non-free, útil na função main-install
+#==============================================================================
+# Funções
+# importante: mostra uma recomendação para o usuário.
+# addsources: Faz a magica, mostra o checklist para escolha dos repositórios.
+# main-install: coloca os repositórios no sources.list
+# segurity-install: adiciona o repositório segurity no sources.list
 
+#==============================================================================
 # Função de inicio do script para informar o usuário
 # sobre questões importantes, antes de configurar os
 # repositório.
@@ -39,7 +60,7 @@ sources=$( dialog --stdout			           \
 		--title 'Seleção de espelhos APT'          \
 		--separate-output			   \
 		--checklist 'Adicionar repositórios' 0 0 0 \
-		Main 	          'Recomendado' on	   \ 
+		Main 	          'Recomendado' on	   \
 		Segurity          'Recomendado' on 	   \
 		Updates           'Recomendado' on   	   \
 		Proposed-updates  'Opcional'    off        \
@@ -53,7 +74,7 @@ sources=$( dialog --stdout			           \
 # estiver marcada.
 echo $sources | grep -q 'Non-free' && NONFREE=non-free CONTRIB=contrib
 
-# O conteúdo de sources, vai ser processado pelo while, linha por linha e -
+# O conteúdo de sources, vai ser processado pelo while, linha por linha e
 # em combinação com o case, será chamada uma função que adicionará o repositório escolhido.
  echo "$sources" | while read escolha
  do
@@ -87,7 +108,7 @@ echo $sources | grep -q 'Non-free' && NONFREE=non-free CONTRIB=contrib
   esac
 done
 
-# Após o fim do loop, as mesagens apareceram e haverá
+# Após o fim do loop, essa mensagem aparecerá e haverá
 # a atualização dos repositórios.
 dialog                                                                \
    --title 'Aguarde...'                                               \
@@ -128,12 +149,12 @@ deb http://deb.debian.org/debian-security/ stretch/updates main $CONTRIB $NONFRE
 # faz o backup do sources.list da maquina e
 # Limpa o arquivo sources.list
 if [ $(id -u) = 0 ]; then
-	cp $ESPELHOS $ESPELHOS${$}.bak
-	ALTERA="> $ESPELHOS"
-	eval $ALTERA
-	importante
+		cp $ESPELHOS $ESPELHOS${$}.bak
+		ALTERA="> $ESPELHOS"
+		eval $ALTERA
+		importante
 else
-#mostra dialógo que o user não é root.
+	#mostra dialógo que o user não é root.
 	{
 		dialog			                    \
 		--title "Sem permissões"                    \
